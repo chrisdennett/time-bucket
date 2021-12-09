13cc39e18d0abdcac7a10473a34c753d8d23e3e5
chrisdennett <chrisdennett@gmail.com>
2021-12-09 16:42:46 +0000

    working smoothly

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ let cam;
 
 let pastFrames = []; // an array to store old frames
 let numFrames = 100; // how many frames to store. We will run out of memory at a certain point so don't make this too big
-let step, windowStep; // the height of our slit strips
 
 // the width and height of our past frames
 // we make them smaller so that we can store more
@@ -14,9 +13,14 @@ let step, windowStep; // the height of our slit strips
 let w = 512;
 let h = 512;
 
+let smallestDiameterSlider;
+let stepsSlider;
+let checkbox;
+let isReversed;
+
 function setup() {
   // shaders require WEBGL mode to work
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
 
   // initialize the webcam at the window size
@@ -26,11 +30,19 @@ function setup() {
   // hide the html element that createCapture adds to the screen
   cam.hide();
 
-  // calculate the height of each horizontal strip
-  // might have to floor this to prevent gaps...
-  step = h / numFrames;
+  smallestDiameterSlider = createSlider(0, 1, 0.2, 0.01);
+  smallestDiameterSlider.position(10, 10);
+  smallestDiameterSlider.style("width", "200px");
 
-  windowStep = height / numFrames;
+  stepsSlider = createSlider(1, 300, 250, 1);
+  stepsSlider.position(10, 40);
+  stepsSlider.style("width", "200px");
+
+  checkbox = createCheckbox("isReversed", false);
+  checkbox.position(10, 70);
+  checkbox.changed(myCheckedEvent);
+
+  isReversed = false;
 
   // for every frame, add a graphics layer
   for (let i = 0; i < numFrames; i++) {
@@ -39,32 +51,49 @@ function setup() {
   }
 }
 
+function myCheckedEvent() {
+  isReversed = this.checked();
+}
+
 function draw() {
+  translate(-width / 2, -height / 2);
+
+  // if number of steps change
+  let totalSteps = stepsSlider.value();
+  if (totalSteps < pastFrames.length) {
+    let removeSteps = pastFrames.length - totalSteps;
+    pastFrames = pastFrames.slice(removeSteps);
+  } else if (totalSteps > pastFrames.length) {
+    let totalNewSteps = totalSteps - pastFrames.length;
+    for (let i = 0; i < totalNewSteps; i++) {
+      let p = createGraphics(w, h);
+      pastFrames.push(p);
+    }
+  }
+
   // draw the current camera frame in the first element of the array
   pastFrames[0].image(cam, 0, 0, w, h);
 
-  // draw our slit scan to the screen
-  // we loop through all the frames and draw a slice at each step along the y axis
-  for (let i = 0; i < pastFrames.length; i++) {
-    // image(img, x, y, w, h, srcX, srcY, srcW, srcH);
-    image(
-      pastFrames[i],
-      0,
-      windowStep * i,
-      width,
-      windowStep,
-      0,
-      step * i,
-      w,
-      step
-    );
-  }
-
   // move every element forward by 1, except the last element
-  // this is important to keep the frames cycling
-  // otherwise we'd just see one frame updating at a time
   for (let i = 0; i < pastFrames.length - 1; i++) {
     pastFrames[i] = pastFrames[i + 1];
+  }
+
+  let smallestDiam = smallestDiameterSlider.value() * width;
+  let largestDiam = width;
+  let diamStep = (largestDiam - smallestDiam) / pastFrames.length;
+
+  translate(width, 0);
+  scale(-1, 1);
+
+  for (let i = 0; i < pastFrames.length; i++) {
+    if (isReversed) {
+      texture(pastFrames[pastFrames.length - i - 1]);
+    } else {
+      texture(pastFrames[i]);
+    }
+
+    circle(width / 2, height / 2, width - i * diamStep);
   }
 
   // move the last element to the beginning
